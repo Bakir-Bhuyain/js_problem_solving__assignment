93a5684ebcfa9c88234177e002737857e228798c
Bakir-Bhuyain <hemalbakir@gmail.com>
2026-01-17 12:59:40 +0600

    completed the problem no1

--- problem1.js
+++ problem1.js
@@ -1,1 +1,18 @@
 //problem statement
+
+function calculateMoney(totalTicket) {
+  //validation remaining:-
+  if (0 <= totalTicket) {
+    const perTicket = 120;
+    const forSecurityGuard = 500;
+    const forStuffLunch = 50;
+    const totalStuff = 8;
+    const stuffCost = forStuffLunch * totalStuff + forSecurityGuard;
+    const remainingCost = perTicket * totalTicket - stuffCost;
+    return remainingCost;
+  } else {
+    return "Invalid Number: Please provide a positive number";
+  }
+}
+const total = calculateMoney(-12);
+console.log(total);
